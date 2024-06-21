83386ef9e66e5216ea7f3a0cdc57ce024d80aa0e
pdouvc-asg614jv\putti <puttipong.dou@gmail.com>
2024-06-22 00:49:57 +0700

    up

--- models/token_blacklist_model.js
+++ models/token_blacklist_model.js
@@ -1,20 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-    const TokenBlacklist = sequelize.define('TokenBlacklist', {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-      },
-      token: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-      },
-    });
-  
-    return TokenBlacklist;
-  };
-  
+  const TokenBlacklist = sequelize.define("TokenBlacklist", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    token: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+
+  return TokenBlacklist;
+};

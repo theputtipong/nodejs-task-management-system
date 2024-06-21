7281e9fdf71e5a8a718765a18cdf84d82395f890
pdouvc-asg614jv\putti <puttipong.dou@gmail.com>
2024-06-21 22:37:15 +0700

    update noti

--- services/push_service.js
+++ services/push_service.js
@@ -1,15 +1,13 @@
-const webPush = require('web-push');
-
-webPush.setVapidDetails(
-    'mailto:your_email@gmail.com',
-    'your_public_vapid_key',
-    'your_private_vapid_key'
-);
+// webPush.setVapidDetails(
+//   "mailto:your_email@gmail.com",
+//   "your_public_vapid_key",
+//   "your_private_vapid_key"
+// );
 
 const sendPush = async (to, title, body) => {
-    const payload = JSON.stringify({ title, body });
+  const payload = JSON.stringify({ title, body });
 
-    await webPush.sendNotification(to, payload);
+  await webPush.sendNotification(to, payload);
 };
 
 module.exports = { sendPush };
--- services/sms_service.js
+++ services/sms_service.js
@@ -1,21 +1,23 @@
-require('dotenv').config();
-const Vonage = require('@vonage/server-sdk')
+require("dotenv").config();
+const { Vonage } = require("@vonage/server-sdk");
 
-const vonage = Vonage({
+const vonage = new Vonage({
   apiKey: process.env.VONAGE_API_KEY,
-  apiSecret: process.env.VONAGE_API_SECRET
+  apiSecret: process.env.VONAGE_API_SECRET,
 });
 
 const sendSMS = (to, text) => {
-  const from = 'YourBrand';
+  const from = "YourBrand";
   vonage.message.sendSms(from, to, text, (err, responseData) => {
     if (err) {
-      console.error('Error:', err);
+      console.error("Error:", err);
     } else {
-      if (responseData.messages[0].status === '0') {
-        console.log('Message sent successfully.');
+      if (responseData.messages[0].status === "0") {
+        console.log("Message sent successfully.");
       } else {
-        console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+        console.log(
+          `Message failed with error: ${responseData.messages[0]["error-text"]}`
+        );
       }
     }
   });

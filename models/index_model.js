0717960bc3d7969b1a5123a07e82a52c57c7fe8b
pdouvc-asg614jv\putti <puttipong.dou@gmail.com>
2024-06-22 22:43:37 +0700

    update config again

--- models/index_model.js
+++ models/index_model.js
@@ -1,6 +1,16 @@
 const Sequelize = require("sequelize");
-const config = require("../config/config.json");
-const sequelize = new Sequelize(config.development);
+const config = require("./config.js");
+const sequelize = new Sequelize(
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    host: config.development.host,
+    dialect: config.development.dialect,
+    port: config.development.port,
+    logging: false, // Set to true if you want to see SQL queries
+  }
+);
 
 const db = {};
 

6318520b127bbd1b23a61f86a6fdcdcc1ac043d0
pdouvc-asg614jv\putti <puttipong.dou@gmail.com>
2024-06-23 00:28:52 +0700

    update cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./models/index_model");
 const taskRoutes = require("./routes/task_routes");
 const authRoutes = require("./routes/auth_routes");
@@ -9,6 +10,20 @@ const setupSwagger = require("./config/swagger");
 const app = express();
 
 app.use(express.json());
+
+const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"], // ระบุ HTTP methods ที่อนุญาต
+  allowedHeaders: ["Content-Type", "Authorization"], // ระบุ header ที่อนุญาต
+};
+app.use(cors(corsOptions));
 
 app.use("/tasks", taskRoutes);
 app.use("/auth", authRoutes);

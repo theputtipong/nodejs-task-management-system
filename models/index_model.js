357d09116151fae31712a430ddb73079df93670e
Puttipong Dou <bank@coindragon.co>
2024-09-26 12:53:00 +0700

    clean some code and update test

--- models/index_model.js
+++ models/index_model.js
@@ -1,22 +1,22 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+// const Sequelize = require("sequelize");
+// require("dotenv").config();
 
-const sequelize = new Sequelize({
-  database: process.env.POSTGRES_DB,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  dialect: process.env.POSTGRES_DIALECT,
-  port: process.env.POSTGRES_PORT,
-});
+// const sequelize = new Sequelize({
+//   database: process.env.POSTGRES_DB,
+//   username: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   host: process.env.POSTGRES_HOST,
+//   dialect: process.env.POSTGRES_DIALECT,
+//   port: process.env.POSTGRES_PORT,
+// });
 
-const db = {};
+// const db = {};
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
 
-db.tasks = require("./task_model")(sequelize, Sequelize);
-db.users = require("./user_model")(sequelize, Sequelize);
-db.TokenBlacklist = require("./token_blacklist_model")(sequelize, Sequelize);
+// db.tasks = require("./task_model")(sequelize, Sequelize);
+// db.users = require("./user_model")(sequelize, Sequelize);
+// db.TokenBlacklist = require("./token_blacklist_model")(sequelize, Sequelize);
 
-module.exports = db;
+// module.exports = db;
